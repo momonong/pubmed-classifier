2e7107d522f94ef00bfabd6e70f65adc78ef38a6
momonong <morrischen2.9@gmail.com>
2025-05-24 10:26:51 +0800

    Update gitignore

--- scripts/test_spacy_model.py
+++
@@ -1,6 +0,0 @@
-import spacy
-
-nlp = spacy.load("en_core_sci_sm")
-doc = nlp("Alterations in the hypocretin receptor 2 and preprohypocretin genes produce narcolepsy in some animals.")
-for token in doc:
-    print(token.text, token.lemma_, token.pos_)
